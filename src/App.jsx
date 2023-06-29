cee8a54a60bb7f535174885b0489b767be0b6ddd
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-06-29 08:54:42 +0330

    Modify the App.js to add routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { Routes, Route } from 'react-router';
 import './App.scss';
+import Show from './pages/Show';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Crypto Panel</h1>
+        <div>Crypto Panel</div>
       </header>
+      <main>
+        <Routes>
+          <Route path="/Cryptopanel/" element={<Home />} />
+          <Route path="/Cryptopanel/:id" element={<Show />} />
+        </Routes>
+      </main>
+      <footer>
+        <p className="copyright">&copy; 2023 Crypto Panel | Developed by Abdulali Noori</p>
+      </footer>
     </div>
   );
 }
