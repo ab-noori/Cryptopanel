c55a05b5f87ac5622386b936a0ed36e44a402db6
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-06-29 12:43:35 +0330

    Modify store to add detail reducer.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import CurrencyReducer from './CurrencySlice';
+import DetailsReducer from './DetailsSlice';
 
 const store = configureStore({
   reducer: {
     Currencies: CurrencyReducer,
+    CurrencyDetails: DetailsReducer,
   },
 });
 
