bdaa68ccdc7cdb311e8cf1faff09d580c34aaadf
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-06-30 23:41:24 +0330

    Update the Home page component to display the general currencies.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCurrency, searchCurrency } from '../redux/CurrencySlice';
+import CurrencyList from '../components/CurrencyList';
 
 const Home = () => {
-  const Currencies = useSelector((state) => state.Currencies.Currencies);
+  const currencies = useSelector((state) => state.Currencies.Currencies);
   const [query, setQuery] = useState('');
   const dispatch = useDispatch();
 
@@ -32,22 +32,12 @@ const Home = () => {
   };
 
   return (
-    <>
-      <h1>Main page</h1>
-      <input
-        type="text"
-        value={query}
-        placeholder="Search"
-        onChange={handleQuery}
-        onKeyDown={handleKeyDown}
-      />
-
-      {Currencies.map((Currency) => (
-        <div className="currency-list" key={Currency.id}>
-          <Link to={`/Cryptopanel/${Currency.id}`}>{Currency.name}</Link>
-        </div>
-      ))}
-    </>
+    <CurrencyList
+      currencies={currencies}
+      query={query}
+      handleQuery={handleQuery}
+      handleKeyDown={handleKeyDown}
+    />
   );
 };
 
